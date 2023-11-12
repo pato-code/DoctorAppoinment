f544ae9439a191b79fff0c169b1d3f3b95a50430
Albraa Hesham <akame.pato@gmail.com>
2023-11-12 18:13:06 +0200

    finlize bottom bar with active state

--- routes/HomeRoute.jsx
+++ routes/HomeRoute.jsx
@@ -3,6 +3,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from '../presentation_layer/HomePage';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import BottomBar from '../components/BottomBar';
+import tw from 'twrnc';
+import { View } from 'react-native';
 
 const Tabs = createBottomTabNavigator()
 
@@ -12,8 +15,22 @@ export default function HomeRoute({ navigation }) {
     return (
         <Tabs.Navigator initialRouteName="HomePage" screenOptions={{
             headerShown: false
-        }}>
-            <Tabs.Screen name="HomePage" component={HomePage}></Tabs.Screen>
+        }}
+        tabBarBackground='white'
+        
+        style={tw.style('bg-white')} >
+             {/* tabBar={props => <BottomBar {...props} />}> */}
+            <Tabs.Screen name="HomePage" component={HomePage} options={{
+                tabBarShowLabel: false,
+      tabBarIcon: ({ color, size , focused }) => (
+        <View style={tw.style('flex justify-center items-center')}>
+            <Ionicons name="home" color={color} size={size} />
+            
+            <View style={tw.style('w-1 h-1 rounded-full bg-[#555FD2]')}></View>
+            
+        </View>
+      ),
+    }}></Tabs.Screen>
         </Tabs.Navigator>
     );
 }
